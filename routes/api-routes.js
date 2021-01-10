df59e6bb2f31d1857a0a487b62cb326761e66be1
Dame <dcampbell7095@gmail.com>
2021-01-09 22:25:03 -0500

    adding get route to populate last workout and sum of total workout duration

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,28 +1,37 @@
 const router = require("express").Router();
 
-const Workout = require("../models/workout")
+const Workout = require("../models/workout");
 
-router.post("/api/workouts", ({body}, res) => {
-    Workout.create(body).then((newWorkout)=>{
-        res.json(newWorkout)
-    }).catch(err => {
-        console.log(err)
-        res.status(400).json(err)
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ]).then((lastWorkout) => {
+      res.json(lastWorkout)
+  });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((newWorkout) => {
+      res.json(newWorkout);
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
-router.put("/api/workouts/:id", (req, res)=>{
-    workoutId = req.params.id;
+router.put("/api/workouts/:id", (req, res) => {
+  workoutId = req.params.id;
 
-    console.log(workoutId)
+  console.log(workoutId);
 
-    Workout.findByIdAndUpdate(
-        {_id: workoutId},
-        {$push: {exercises: req.body}},
-    ).then((updateWorkout) => {
-        res.json(updateWorkout)
-    })
+  Workout.findByIdAndUpdate(
+    { _id: workoutId },
+    { $push: { exercises: req.body } }
+  ).then((updateWorkout) => {
+    res.json(updateWorkout);
+  });
+});
 
-})
-
-module.exports = router
+module.exports = router;
